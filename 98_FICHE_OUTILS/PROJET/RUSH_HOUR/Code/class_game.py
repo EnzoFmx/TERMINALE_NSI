9cfee798d319e3ff9eabd362fdb9441d389780e2
EnzoFmx <enzofremeaux@gmail.com>
2024-02-05 11:04:23 +0100

    ajout auto script

--- 98_FICHE_OUTILS/PROJET/RUSH_HOUR/Code/class_game.py
+++ 98_FICHE_OUTILS/PROJET/RUSH_HOUR/Code/class_game.py
@@ -1,7 +1,7 @@
 import class_vehicule
 class Game :
 
-    def __init__(self,nb_col,vehicules = []) :
+    def __init__(self,nb_col,vehicules) :
         """Class permettant de gerer la classe game, qui permet de gerer le plateau
 
         Args:
@@ -10,7 +10,10 @@ class Game :
         self.plateau = [[' ' for _ in range(nb_col)] for _ in range(nb_col)]
         self.taille = len(self.plateau)
         # Liste des véhicules dans le tab (pour modifier/ selectionner)
-        self.vehicules = vehicules
+        self.vehicule = []
+        for element in vehicules :
+            self.pose_vehicule(element)
+            
     
     def verif_pose(self,vehicule):
         if vehicule.direction == 'V' and ((vehicule.coord_y + vehicule.taille) <= len(self.plateau)):
@@ -37,13 +40,71 @@ class Game :
                     self.plateau[vehicule.coord_y][vehicule.coord_x+i] = vehicule.nom    
         else :
             print('Attention pose du véhicule, ',vehicule,' impossible')
-    
- 
+    """
+    def remplace_vehicule(self,nom,v2):
+        for i in self.vehicules :
+            if i.nom == 
+    def deplacement_possible(self,i) :
+        if i.direction == 'H' :
+            # Test du déplacement haut :
+                if i.coord_y-1 < 0 and (self.plateau[i.coord_y-1][i.coord_x] == ' ') :
+                    v_haut = (Vehicule(i.type_v,i.direction,i.coord_x,i.coord_y-1,i.nom),i.nom + ' Haut')
+                if i.coord_y+1 < self.taille and (self.plateau[i.coord_y+1][i.coord_x] == ' ') :
+                    v_bas= (Vehicule(i.type_v,i.direction,i.coord_x,i.coord_y+1,i.nom),i.nom + ' Bas')
+        else :
+                if i.coord_x-1 < 0 and (self.plateau[i.coord_y][i.coord_x-1] == ' ') :
+                    v_gauche = (Vehicule(i.type_v,i.direction,i.coord_x,i.coord_y-1,i.nom),i.nom + ' Haut')
+                if i.coord_x+1 < self.taille and (self.plateau[i.coord_y][i.coord_x+1] == ' ') :
+                    v_droite= (Vehicule(i.type_v,i.direction,i.coord_x,i.coord_y+1,i.nom),i.nom + ' Bas')
+
+                # Test du déplacement bas :
+        """
 
         
         
-
-
+    def mouv_vehicule(self,vehicule):
+        solution = []
+        if vehicule.direction == 'H' :
+            # Mouv droite :
+            if (vehicule.coord_x+1 < self.taille) and (self.plateau[vehicule.coord_y][vehicule.coord_x+vehicule.taille] == ' '):
+                # creer un nouveau jeu avec le nouveau vehicule
+                vehicule2 = []
+                for element in self.vehicules :
+                    if element.nom != vehicule.nom :
+                        vehicule2.append(element)
+                v2 = Vehicule(vehicule.type_v,vehicule.direction,vehicule.coord_x+1,vehicule.coord_y,vehicule.nom)
+                vehicules2.append(v2)
+                solution.append((Game(self.taille,vehicules2),v2.nom + ' droite'))
+            # Mouv gauche :
+            if (vehicule.coord_x-1 > 0) and (self.plateau[vehicule.coord_y][vehicule.coord_x-1] == ' '):
+                vehicule2 = []
+                for element in self.vehicules :
+                    if element.nom != vehicule.nom :
+                        vehicule2.append(element)
+                v2 = Vehicule(vehicule.type_v,vehicule.direction,vehicule.coord_x-1,vehicule.coord_y,vehicule.nom)
+                vehicules2.append(v2)
+                solution.append((Game(self.taille,vehicules2),v2.nom + ' gauche'))
+        else :
+            # Mouv haut
+            if (vehicule.coord_y+1 < self.taille) and (self.plateau[vehicule.coord_y+vehicule.taille][vehicule.coord_x] == ' '):
+                vehicule2 = []
+                for element in self.vehicules :
+                    if element.nom != vehicule.nom :
+                        vehicule2.append(element)
+                v2 = Vehicule(vehicule.type_v,vehicule.direction,vehicule.coord_x,vehicule.coord_y+1,vehicule.nom)
+                vehicules2.append(v2)
+                solution.append((Game(self.taille,vehicules2),v2.nom + ' haut'))
+            
+            # Mouv bas 
+            if (vehicule.coord_y-1 > 0) and (self.plateau[vehicule.coord_y-1][vehicule.coord_x] == ' '):
+                vehicule2 = []
+                for element in self.vehicules :
+                    if element.nom != vehicule.nom :
+                        vehicule2.append(element)
+                v2 = Vehicule(vehicule.type_v,vehicule.direction,vehicule.coord_x,vehicule.coord_y-1,vehicule.nom)
+                vehicules2.append(v2)
+                solution.append((Game(self.taille,vehicules2), v2.nom + ' bas'))
+        return solution
 
 if __name__ == '__main__' :
     g = Game(6)
